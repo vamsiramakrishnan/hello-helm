21ccb1d1517144bcf5d0dce7ee1efc7fad09d3ca
Vamsi Ramakrishnan <vamsiramakrishnan@gmail.com>
2018-09-11 16:29:31 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var app = express();
-var toolchain = {toolchain:["Oracle + Wercker","Kubernetes","Docker","Oracle Kubernetes Engine","Oracle Container Registry"]}
+var pjson = require('./package.json');
+var toolchain = {toolchain:["Oracle + Wercker","Kubernetes","Docker","Oracle Kubernetes Engine","Oracle Container Registry", pjson.version]}
 
 app.get('/', function(req, res){
     res.writeHead(200, { 'Content-Type': 'application/json' });
